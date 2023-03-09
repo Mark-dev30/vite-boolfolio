c1355a7a94b7c487da94c938b5956788b4dc0bc6
Mark-dev30 <fronterremarco@gmail.com>
2023-03-09 18:15:05 +0100

    Milestone 4 completed

--- src/components/ProjectMain.vue
+++ src/components/ProjectMain.vue
@@ -16,8 +16,10 @@ export default {
             this.loading = true;
             axios.get(`${this.baseUrl}/api/projects`).then((response) => {
                 if (response.data.success) {
-                    this.projects = response.data.results
+                    console.log(response.data)
+                    this.projects = response.data.projects
                     this.loading = false;
+
                 }
                 else {
 
@@ -32,11 +34,11 @@ export default {
 </script>
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-12 d-flex flew-wrap">
-                <div class="card" style="width: 18rem;" v-for="project in projects" :key="project.id">
-                    <img :src="project.cover_image != null ? project.cover_image : 'https://unsplash.com/it/foto/WtolM5hsj14'"
-                        class="card-img-top" alt="">
+        <div class="row ">
+            <div class="col-12 d-flex flex-wrap justify-content-center">
+                <div class="card m-2" style="width: 18rem;" v-for="project in projects" :key="project.id">
+                    <img :src="project.cover_image != null ? project.cover_image : 'https://picsum.photos/300/200'"
+                        class="img-fluid" alt="">
                     <div class="card-body">
                         <h5 class="card-title">{{ project.title }}</h5>
                         <p class="card-text">{{ project.description }}</p>
