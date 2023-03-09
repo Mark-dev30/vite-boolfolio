72659a4335f07dcb22dedfcbd18a11285640da89
Mark-dev30 <fronterremarco@gmail.com>
2023-03-09 17:03:15 +0100

    Milestone 4 error

---
+++ src/components/ProjectMain.vue
@@ -0,0 +1,42 @@
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ProjectMain',
+    data() {
+        return {
+            projects: [],
+            loading: true,
+            baseUrl: 'http://127.0.0.1:8000'
+        }
+    },
+    methods: {
+        getPojects() {
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/projects`).then((response) => {
+                if (response.data.success) {
+                    this.projects = response.data.results
+                    this.loading = false;
+                }
+                else {
+
+                }
+            });
+        }
+    },
+    mounted() {
+        this.getPojects()
+    }
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped></style>
